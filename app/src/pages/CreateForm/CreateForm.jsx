fa996901512052db1a5c314e03dbb385bd8bfda9
dharun <dharundds@gmail.com>
2021-05-30 13:46:17 +0530

    updates

--- app/src/pages/CreateForm/CreateForm.jsx
+++ app/src/pages/CreateForm/CreateForm.jsx
@@ -1,5 +1,23 @@
+import './CreateForm.css'
+
 function CreateForm() {
-  return <div className="CreateForm">Create Form</div>;
+  return (
+  <div className="CreateForm">
+    <input type="text" id='form_name' placeholder='Form Name' autoComplete='off' />
+    <div className="fields">
+    <span>
+      <input type="text" id='question_field' placeholder='Enter your question' autoComplete='off' />
+      <select name="inputs" id="dropdown">
+        <option value="Text">Text</option>
+        <option value="options">options</option>
+        <option value="dropdown">dropdown</option>
+      </select>
+      </span>
+      <input type='text' id='answer_input' placeholder="Enter your answer" />
+    </div>
+    <button type='button'>Add field</button>
+  </div>
+  );
 }
 
 export default CreateForm;
