c85ca4fb06947e190fee8712e13e74a50f3092b8
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-05-30 14:22:43 +0530

    Updates

--- app/src/pages/CreateForm/CreateForm.jsx
+++ app/src/pages/CreateForm/CreateForm.jsx
@@ -1,22 +1,31 @@
-import './CreateForm.css'
+import "./CreateForm.css";
+import { Plus } from "../../components/Symbols";
 
 function CreateForm() {
   return (
-  <div className="CreateForm">
-    <input type="text" id='form_name' placeholder='Form Name' autoComplete='off' />
-    <div className="fields">
-    <span>
-      <input type="text" id='question_field' placeholder='Enter your question' autoComplete='off' />
-      <select name="inputs" id="dropdown">
-        <option value="Text">Text</option>
-        <option value="options">options</option>
-        <option value="dropdown">dropdown</option>
-      </select>
-      </span>
-      <input type='text' id='answer_input' placeholder="Enter your answer" />
+    <div className="CreateForm">
+      <input type="text" className="form-name" placeholder="Form name" />
+      <div className="fields">
+        {/* <span>
+          <input
+            type="text"
+            id="question_field"
+            placeholder="Enter your question"
+            autoComplete="off"
+          />
+          <select name="inputs" id="dropdown">
+            <option value="Text">Text</option>
+            <option value="options">options</option>
+            <option value="dropdown">dropdown</option>
+          </select>
+        </span>
+        <input type="text" id="answer_input" placeholder="Enter your answer" /> */}
+      </div>
+      <button className="add-field-button">
+        <Plus size="20" fill="#66fcf1" />
+        <span>Add field</span>
+      </button>
     </div>
-    <button type='button'>Add field</button>
-  </div>
   );
 }
 
--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
+import { useHistory } from "react-router-dom";
 import "./Home.css";
 import { Plus } from "../../components/Symbols";
 
 function Home() {
+  const history = useHistory();
+
   return (
     <div className="Home">
       <div className="templates-container">
-        <div className="create-form template">
+        <div
+          className="create-form template"
+          onClick={() => {
+            history.push("/createform");
+          }}
+        >
           <Plus fill="#66fcf1" size="32" />
         </div>
         <div className="template1 template"></div>
