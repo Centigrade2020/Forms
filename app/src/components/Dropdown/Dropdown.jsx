596ac1acffe3b3bf8092b7dafac8f21093b572f4
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-05-31 12:20:52 +0530

    Updates

--- app/src/components/Dropdown/Dropdown.jsx
+++ app/src/components/Dropdown/Dropdown.jsx
@@ -7,15 +7,25 @@ const Dropdown = () => {
   const [selected, setSelected] = useState(opts[0]);
   return (
     <div className="Dropdown">
-      <div className="display">
+      <div
+        className="display"
+        id="display"
+        onClick={() => {
+          document.getElementById("options").classList.toggle("options-active");
+          document.getElementById("display").classList.toggle("display-active");
+        }}
+      >
         <p>{selected}</p>
         <Symbols.Arrow fill="#c5c6c7" size="24" />
       </div>
-      <div className="options">
+      <div className="options" id="options">
         {opts.map((i) => (
           <p
             onClick={() => {
               setSelected(i);
+              document
+                .getElementById("options")
+                .classList.toggle("options-active");
             }}
           >
             {i}
