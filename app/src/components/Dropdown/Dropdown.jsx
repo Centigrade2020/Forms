d1e7613487460d6defece478d2ddb66d2f3fbb14
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-05-31 12:12:21 +0530

    Updates

--- app/src/components/Dropdown/Dropdown.jsx
+++ app/src/components/Dropdown/Dropdown.jsx
@@ -1,7 +1,29 @@
 import "./Dropdown.css";
+import { Symbols } from "../Symbols";
+import { useState } from "react";
 
 const Dropdown = () => {
-  return <div>Dropdown</div>;
+  const opts = ["Text", "Multiple Choice", "Check Box", "Dropdown"];
+  const [selected, setSelected] = useState(opts[0]);
+  return (
+    <div className="Dropdown">
+      <div className="display">
+        <p>{selected}</p>
+        <Symbols.Arrow fill="#c5c6c7" size="24" />
+      </div>
+      <div className="options">
+        {opts.map((i) => (
+          <p
+            onClick={() => {
+              setSelected(i);
+            }}
+          >
+            {i}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
