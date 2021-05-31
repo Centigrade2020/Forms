368580c9dc89cdf6efafd3615e9ef0a6bcad767d
dharun <dharundds@gmail.com>
2021-05-31 15:07:56 +0530

    Update CreateForm.jsx

--- app/src/pages/CreateForm/CreateForm.jsx
+++ app/src/pages/CreateForm/CreateForm.jsx
@@ -1,34 +1,35 @@
 import "./CreateForm.css";
 import { Symbols } from "../../components/Symbols";
 import { FormField } from "../../components/FormField";
+import { useState } from "react";
+
+const AddedElement = ()=> <div className="fields"><FormField /></div>
 
 function CreateForm() {
+  const [addfield,setaddfield] = useState(0)
   return (
     <div className="CreateForm">
       <input type="text" className="form-name" placeholder="Form name" />
       <div className="fields">
         <FormField />
-        {/* <span>
-          <input
-            type="text"
-            id="question_field"
-            placeholder="Enter your question"
-            autoComplete="off"
-          />
-          <select name="inputs" id="dropdown">
-            <option value="Text">Text</option>
-            <option value="options">options</option>
-            <option value="dropdown">dropdown</option>
-          </select>
-        </span>
-        <input type="text" id="answer_input" placeholder="Enter your answer" /> */}
+       
       </div>
-      <button className="add-field-button">
+      {[...Array(addfield)].map((i)=>
+      <AddedElement key={i}/>
+      )}
+      { addfield>0 ?  <button className="add-field-button" onClick={()=>setaddfield(addfield-1)}>Remove field</button> : null}
+      
+      <br />
+      <button className="add-field-button" onClick={()=> setaddfield(addfield+1)}>
         <Symbols.Plus size="20" fill="#66fcf1" />
         <span>Add field</span>
       </button>
+      
     </div>
+    
   );
 }
 
 export default CreateForm;
+
+
