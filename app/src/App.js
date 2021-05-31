e1b83fe9f1713cd152a6f2db006ddfb9845f0dd8
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-05-31 16:36:04 +0530

    Updates

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,23 @@ import { NavBar } from "./components/NavBar";
 import Home from "./pages/Home";
 import CreateForm from "./pages/CreateForm";
 
+import { useAuth, useResolved } from "./hooks";
+
 function App() {
+  const history = useHistory();
+  const authUser = useAuth();
+  const authResolved = useResolved(authUser);
+
+  useEffect(() => {
+    if (authResolved) {
+      history.push(!!authUser ? "/" : "login");
+    }
+  }, [authResolved, authUser, history]);
+
+  useEffect(() => {
+    console.log(authUser, authResolved);
+  }, [authUser, authResolved]);
+
   return (
     <Router>
       <NavBar />
